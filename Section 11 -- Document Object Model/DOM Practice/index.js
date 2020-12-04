66b655de469f805c2c5813e9b78d6bc099593888
Daylen-Mackey <dmackey@ualberta.ca>
2020-12-03 23:03:19 -0700

    Reviewing the Document Object Model with some simple exercises

---
+++ Section 11 -- Document Object Model/DOM Practice/index.js
@@ -0,0 +1,9 @@
+alert("Hit Enter to overwrite header, click checkbox, and change third list text")
+
+
+document.firstElementChild.lastElementChild.firstElementChild.innerHTML = "Overwritten"
+
+
+document.querySelector("input").click()
+
+document.querySelector("ul").lastElementChild.innerText = "My name jeff"
